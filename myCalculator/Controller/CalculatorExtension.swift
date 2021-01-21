392d0ef7616ebec45e0ae9fcbbdd2f37b8dd8b59
indratdev <indrat.dev@gmail.com>
2021-01-21 23:27:38 +0700

    penambahan rounded button function

--- myCalculator/Controller/CalculatorExtension.swift
+++ myCalculator/Controller/CalculatorExtension.swift
@@ -7,4 +7,17 @@
 //
 
 import Foundation
+import UIKit
 
+    extension UIButton{
+        func roundedButton(){
+            let maskPath1 = UIBezierPath(roundedRect: bounds,
+                byRoundingCorners: [.topLeft , .topRight],
+                cornerRadii: CGSize(width: 8, height: 8))
+            let maskLayer1 = CAShapeLayer()
+            maskLayer1.frame = bounds
+            maskLayer1.path = maskPath1.cgPath
+            layer.mask = maskLayer1
+        }
+    
+}
