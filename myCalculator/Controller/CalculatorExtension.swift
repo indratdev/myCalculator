c0c5ba71b28aa8caded0f84cb88565ce7769bc9e
indratdev <indrat.dev@gmail.com>
2021-01-22 19:57:46 +0700

    perubahan fungsi di number button

--- myCalculator/Controller/CalculatorExtension.swift
+++ myCalculator/Controller/CalculatorExtension.swift
@@ -9,15 +9,31 @@
 import Foundation
 import UIKit
 
-    extension UIButton{
-        func roundedButton(){
-            let maskPath1 = UIBezierPath(roundedRect: bounds,
-                byRoundingCorners: [.topLeft , .topRight],
-                cornerRadii: CGSize(width: 8, height: 8))
-            let maskLayer1 = CAShapeLayer()
-            maskLayer1.frame = bounds
-            maskLayer1.path = maskPath1.cgPath
-            layer.mask = maskLayer1
-        }
+struct CalculatorExtension {
+    let vc = ViewController()
+    
+    
+    func resetAC(){
+        vc.firstNumber = ""
+        vc.secondNumber = ""
+        vc.operation = false
+        vc.operationSymbol  = ""
+        vc.result = 0.0
+        print("Reset All Running.....")
+    }
     
 }
+
+extension UIButton{
+    func roundedButton(){
+        let maskPath1 = UIBezierPath(roundedRect: bounds,
+                                     byRoundingCorners: [.topLeft , .topRight],
+                                     cornerRadii: CGSize(width: 8, height: 8))
+        let maskLayer1 = CAShapeLayer()
+        maskLayer1.frame = bounds
+        maskLayer1.path = maskPath1.cgPath
+        layer.mask = maskLayer1
+    }
+    
+}
+
